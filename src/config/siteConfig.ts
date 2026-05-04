e8d6ea060f4dbf4c728d5d4e7a42c6162d157e24
lanye-fengren <lanye20h@qq.com>
2026-05-04 15:35:18 +0800

    更新站点配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -13,7 +13,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "澜晔lan",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.lanyeh.top",
 
 	// 站点描述
 	description:
